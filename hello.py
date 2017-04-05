9c035da42325cf7efa6f5be1792574107ff7b0ec
lazarus0011 <lazarus.zhu@gmail.com>
2017-04-05 14:57:04 +0800

    '20170405'

---
+++ hello.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+import re
+import asyncio
+import time
+
+@asyncio.coroutine
+def f():
+	time.sleep(1)
+	print('world')
+	return "abc"
+	
+	
+
+@asyncio.coroutine
+def hello():
+	print('hello')
+#	r = yield from asyncio.sleep(3)
+	r = yield from f()
+	print(r)
+	print('hello again')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(hello())
+print('end')
+loop.close()
